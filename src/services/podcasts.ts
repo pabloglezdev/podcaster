c96dca95220abf1eb388601693a2b7f813ef9220
Pablo Gonzalez Gonzalez <pablo.glez.dev@gmail.com>
2024-04-25 13:26:10 +0100

    ADD podcasts services and CLEAN files

---
+++ src/services/podcasts.ts
@@ -0,0 +1,17 @@
+export const getPodcasts = async (limit: number) =>
+  await fetch(`https://itunes.apple.com/us/rss/toppodcasts/limit=${limit}/genre=1310/json`)
+    .then((response) => {
+      if (response.ok) return response.json();
+      throw new Error(`Network response was not ok with code ${response.status}}`);
+    })
+    .then((data) => data);
+
+export const getPodcastDetails = async (id: string) =>
+  await fetch(
+    `https://api.allorigins.win/get?url=${encodeURIComponent(`https://itunes.apple.com/lookup?id=${id}&media=podcast &entity=podcastEpisode&limit=20`)}`
+  )
+    .then((response) => {
+      if (response.ok) return response.json();
+      throw new Error(`Network response was not ok with code ${response.status}}`);
+    })
+    .then((data) => JSON.parse(data.contents));
