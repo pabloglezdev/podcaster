b09ff8c2995468feb98dbb68daa0d6f7554de4f0
Pablo Gonzalez Gonzalez <pablo.glez.dev@gmail.com>
2024-04-25 16:24:57 +0100

    Clean up and small code refactors

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,4 @@
-import { useEffect } from 'react';
-import { getPodcastDetails, getPodcasts } from './services/podcasts';
-
 function App() {
-  useEffect(() => {
-    const fetchPodcasts = async () => {
-      const podcasts = await getPodcasts(100);
-      console.log(podcasts);
-    };
-
-    const fetchPodcastsDetail = async () => {
-      const podcastsDetail = await getPodcastDetails('934552872');
-      console.log(podcastsDetail);
-    };
-
-    fetchPodcasts();
-    fetchPodcastsDetail();
-  }, []);
-
   return (
     <>
       <h4>Podcaster</h4>
--- src/services/podcasts.ts
+++ src/services/podcasts.ts
@@ -1,7 +1,9 @@
 export const getPodcasts = async (limit: number) =>
   await fetch(`https://itunes.apple.com/us/rss/toppodcasts/limit=${limit}/genre=1310/json`)
     .then((response) => {
-      if (response.ok) return response.json();
+      if (response.ok) {
+        return response.json();
+      }
       throw new Error(`Network response was not ok with code ${response.status}}`);
     })
     .then((data) => data);
@@ -11,7 +13,9 @@ export const getPodcastDetails = async (id: string) =>
     `https://api.allorigins.win/get?url=${encodeURIComponent(`https://itunes.apple.com/lookup?id=${id}&media=podcast &entity=podcastEpisode&limit=20`)}`
   )
     .then((response) => {
-      if (response.ok) return response.json();
+      if (response.ok) {
+        return response.json();
+      }
       throw new Error(`Network response was not ok with code ${response.status}}`);
     })
     .then((data) => JSON.parse(data.contents));
