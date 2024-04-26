6fad74162b486d7de6160418687c5e8bf7c739f2
Pablo Gonzalez Gonzalez <pablo.glez.dev@gmail.com>
2024-04-26 23:53:33 +0100

    REFINEMENT visual on header

--- src/components/divider/Divider.tsx
+++ src/components/divider/Divider.tsx
@@ -1,5 +1,5 @@
 import { type FC } from 'react';
 
-const Divider: FC = () => <hr className="border-t border-gray-400 my-2 mx-1" />;
+const Divider: FC = () => <hr className="border-t border-gray-400 mb-4 mx-3" />;
 
 export default Divider;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -10,8 +10,8 @@ const Header: FC<HeaderProps> = (props) => {
   const { title, isLoading = true } = props;
 
   return (
-    <header className="h-[50px] flex justify-between items-center mx-6 mt-2">
-      <h1 className="text-xl">{title || 'Title'}</h1>
+    <header className="h-[50px] flex justify-between items-center mx-4 mt-2">
+      <h1 className="text-xl text-[#8dafd3]">{title || 'Title'}</h1>
       {isLoading && <Loader />}
     </header>
   );
