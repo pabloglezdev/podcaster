c4dccfde22676d901876c2ce49658bac0b5caba4
Pablo Gonzalez Gonzalez <pablo.glez.dev@gmail.com>
2024-04-28 00:16:16 +0100

    FIX inner content styles

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -20,9 +20,9 @@ const Card: FC<CardProps> = (props) => {
       className="w-full border border-gray-200 rounded-sm shadow-[0px_5px_5px_0px] shadow-gray-200 relative inline-flex flex-col items-center h-auto max-h-[200px] p-1 mb-10 cursor-pointer"
       onClick={handleClick}
     >
-      <img className="absoulte top-0 left-0 -translate-y-7 rounded-full" src={image} alt={title} width={90} />
-      <span className="text-center line-clamp-2">{title.toUpperCase()}</span>
-      <span className="text-center text-gray-400 text-s line-clamp-2">Author: {artist}</span>
+      <img className="absoulte top-0 left-0 rounded-full -translate-y-8" src={image} alt={title} width={100} />
+      <span className="text-center line-clamp-2 -translate-y-4">{title.toUpperCase()}</span>
+      <span className="text-center text-gray-400 text-s line-clamp-2 -translate-y-4">Author: {artist}</span>
     </div>
   );
 };
