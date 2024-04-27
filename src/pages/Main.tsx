15aa476752063d792bf0bad29ef89bf6c345659c
Pablo Gonzalez Gonzalez <pablo.glez.dev@gmail.com>
2024-04-27 12:26:28 +0100

    ADD cache to podcasts

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -7,10 +7,37 @@ const Main: FC = () => {
   const [podcasts, setPodcasts] = useState<Podcast[]>([]);
 
   useEffect(() => {
+    const lastFetchedAt = localStorage.getItem('podcasts-fetched-at');
+
+    if (lastFetchedAt) {
+      const fetchedAt = new Date(lastFetchedAt);
+      const now = new Date();
+      const diff = now.getTime() - fetchedAt.getTime();
+      const diffInHours = diff / 1000 / 60 / 60;
+
+      if (diffInHours > 24) {
+        localStorage.removeItem('podcasts-fetched-at');
+        localStorage.removeItem('podcasts');
+      }
+    }
+  }, []);
+
+  useEffect(() => {
     const fetchPodcasts = async () => {
       const data = await getPodcasts(100);
-      setPodcasts(data);
+      if (data) {
+        localStorage.setItem('podcasts-fetched-at', new Date().toISOString());
+        localStorage.setItem('podcasts', JSON.stringify(data));
+        setPodcasts(data);
+      }
     };
+
+    const cachedPodcasts = localStorage.getItem('podcasts');
+    if (cachedPodcasts) {
+      setPodcasts(JSON.parse(cachedPodcasts));
+      return;
+    }
+
     fetchPodcasts();
   }, []);
 
