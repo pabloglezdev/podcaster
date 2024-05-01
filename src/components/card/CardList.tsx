8ef5472c38292eeac2f32cd1433d7a4de147f9ea
Pablo Gonzalez Gonzalez <pablo.glez.dev@gmail.com>
2024-05-01 10:13:39 +0100

    Fix visual bug on card list

--- src/components/card/CardList.tsx
+++ src/components/card/CardList.tsx
@@ -12,9 +12,8 @@ const CardsList: FC<CardsListProps> = (props) => {
 
   const handleClick = (podcast: Podcast) => onClick(podcast);
 
-  // BUG When there are two grid rows, the row height is not respected.
   return (
-    <div id="card-list" className="grid grid-cols-6 grid-rows-auto gap-4 h-[1000px] max-h-[1000px] mt-10">
+    <div id="card-list" className="grid grid-cols-6 grid-rows-auto gap-4 mt-10">
       {cards.map((podcast) => (
         <div key={podcast.id.attributes['im:id']} className="h-auto w-full">
           <Card
